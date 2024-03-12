e89f4475d5cd0abda815cc28cbb4819e0a188051
EDWIN <EDWIN@WINDOWS-NHGN5BN>
2024-03-12 09:53:50 -0500

    segundo commit

--- Veterinary/Veterinary.Shared/Entities/Owner.cs
+++ Veterinary/Veterinary.Shared/Entities/Owner.cs
@@ -1,14 +1,32 @@
-﻿namespace Veterinary.Shared.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Veterinary.Shared.Entities
 {
     public class Owner
     {
 
         public int Id { get; set; }
+
+        [Display(Name = "Documento")]
+        [MaxLength(20, ErrorMessage = "No se permiten más de 20 caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
         public string Document { get; set; }
-        public string FirstName { get; set; }    
+
+        [Display(Name = "Nombre")]
+        [MaxLength(50, ErrorMessage = "No se permiten más de 50 caracteres")]
+        [Required(ErrorMessage = "El campo  {0} es obligatorio")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Apellido")]
+        [MaxLength(50, ErrorMessage = "No se permiten más de 50 caracteres")]
+        [Required(ErrorMessage = "El campo  {0} es obligatorio")]
 
         public string LastName { get; set; }
 
+        [Display(Name = "Email")]
+        [MaxLength(50, ErrorMessage = "No se permiten más de 50 caracteres")]
+        [Required(ErrorMessage = "El campo  {0} es obligatorio")]
+        [EmailAddress(ErrorMessage = "Digite un email valido")]
         public string Email { get; set; }
 
         public string FixedPhone { get; set; }
